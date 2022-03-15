fc367aec4c5852a75898839cdfc190b743084e2b
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2022-03-16 03:31:33 +0600

    connected heroku server

---
+++ src/components/AddNewBlog/AddNewBlog.js
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+const AddNewBlog = ({ openModal, handleModalClose, stories }) => {
+    const blogDate = new Date();
+    const initialBlogInfo = {
+        id: (stories.length + 1).toString(), title: "New Blog", body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea delectus quia nisi magni fuga nobis.", type: "long", image: 'NULL', datetime: blogDate
+    }
+    const [blogInfo, setBlogInfo] = useState(initialBlogInfo);
+
+    const handleOnBlur = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newBlogInfo = { ...initialBlogInfo }
+        newBlogInfo[field] = value;
+        setBlogInfo(newBlogInfo);
+    }
+
+
+    const handleSubmit = e => {
+
+        // console.log(blogInfo);
+
+        fetch('https://frozen-river-03960.herokuapp.com/stories', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(blogInfo)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+
+
+        handleModalClose();
+        e.preventDefault();
+    }
+
+    return (
+        <div>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={openModal}
+                onClose={handleModalClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={openModal}>
+                    <Box sx={style}>
+                        <Typography id="transition-modal-title" variant="h6" component="h2">
+                            Add New Blog
+                        </Typography>
+                        <form onSubmit={handleSubmit} >
+
+                            <TextField
+                                sx={{ width: '90%', m: 1 }}
+                                name="title"
+                                onBlur={handleOnBlur}
+                                defaultValue="Blog Title"
+                                size="small"
+                            />
+                            <TextField
+                                sx={{ width: '90%', m: 1 }}
+                                name="type"
+                                onBlur={handleOnBlur}
+                                defaultValue="Blog Type"
+                                size="small"
+                            />
+                            <TextField
+                                sx={{ width: '90%', m: 1 }}
+                                name="body"
+                                onBlur={handleOnBlur}
+                                defaultValue="Blog Paragraph"
+                                size="small"
+                            />
+                            <TextField
+                                sx={{ width: '90%', m: 1 }}
+                                name="image"
+                                onBlur={handleOnBlur}
+                                defaultValue="Image Link"
+                                size="small"
+                            />
+                            <TextField
+                                disabled
+                                sx={{ width: '90%', m: 1 }}
+                                defaultValue={blogDate.toLocaleDateString()}
+                                size="small"
+                            />
+
+                            <Button type='submit' variant="inherit">Submit</Button>
+
+                        </form>
+                    </Box>
+                </Fade>
+            </Modal>
+        </div>
+    );
+};
+
+export default AddNewBlog;
--- src/components/Stories/Stories.js
+++ src/components/Stories/Stories.js
@@ -2,25 +2,30 @@ import React, { useEffect, useState } from 'react';
 import Switch from '@mui/material/Switch';
 import Grid from '@mui/material/Grid';
 import Story from '../Story/Story';
-import Box from '@mui/material/Box';
+import Alert from '@mui/material/Alert';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import BookIcon from '@mui/icons-material/Book';
 import Button from '@mui/material/Button';
 import './Stories.css';
+import AddNewBlog from '../AddNewBlog/AddNewBlog';
 
 
 const Stories = () => {
+    const [openModal, setOpenModal] = React.useState(false);
+    const handleModalOpen = () => setOpenModal(true);
+    const handleModalClose = () => setOpenModal(false);
+    const [success, setSuccess] = useState(false);
 
     const [allStories, setAllStories] = useState([]);
     const [toggleInput, setToggleInput] = useState({ longToggle: false, shortToggle: false });
 
     useEffect(() => {
-        fetch('http://localhost:5000/stories')
+        fetch('https://frozen-river-03960.herokuapp.com/stories')
             .then(res => res.json())
             .then(data => setAllStories(data));
     }, []);
 
-    const longStories = allStories.filter(story => story.type === "long");
+    const longStories = allStories.filter(story => story.type !== "short");
     const shortStories = allStories.filter(story => story.type === "short");
 
     const handleChange = e => {
@@ -32,85 +37,83 @@ const Stories = () => {
     };
 
     const handleDeleteAll = () => {
-        window.confirm('Are you sure you want to delete ?');
+        if (window.confirm('Are you sure you want to delete ? This action can not be undone!')) {
+            fetch('https://frozen-river-03960.herokuapp.com/stories', {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        setSuccess(true);
+                    }
+                })
+        }
     }
 
     return (
-        <div className='landing-page'>
-            <Grid sx={{ marginTop: 10, backgroundColor: '#242527', width: '80%', mx: 'auto', p: 2 }} container spacing={0}>
-                <Grid item xs={12} sm={8} md={8} lg={8}>
-                    <Button sx={{ fontWeight: 'bold' }} variant="inherit">
-                        <BookIcon sx={{ borderRadius: '50%', backgroundColor: '#3A3B3D', mr: 1, padding: '5px' }} />
-                        Blog</Button>
+        <>
+            <div className='landing-page'>
+                <Grid sx={{ marginTop: 10, backgroundColor: '#242527', width: '80%', mx: 'auto', p: 2 }} container spacing={0}>
+                    <Grid item xs={12} sm={10} md={10} lg={10}>
+                        <Button sx={{ fontWeight: 'bold' }} variant="inherit">
+                            <BookIcon sx={{ borderRadius: '50%', backgroundColor: '#3A3B3D', mr: 1, padding: '5px' }} />
+                            Blog</Button>
 
-                    <Button onClick={handleDeleteAll} sx={{ color: '#bf483b' }} variant="inherit">Delete All</Button>
+                        <Button onClick={handleDeleteAll} sx={{ color: '#bf483b' }} variant="inherit">Delete All</Button>
 
-                    <div style={{ display: 'inline-block', margin: '0 10px' }}>
-                        <FormControlLabel control={<Switch
-                            checked={toggleInput?.longToggle}
-                            name="longToggle"
-                            onChange={handleChange}
-                            inputProps={{ 'aria-label': 'controlled' }}
-                        />} label="Long Stories" />
-                        <h4 style={{ display: 'inline', borderRadius: '50%', backgroundColor: '#18191B', padding: '5px 10px' }}>{longStories.length}</h4>
-                    </div>
-                    <div style={{ display: 'inline-block', margin: '0 10px' }}>
-                        <FormControlLabel control={<Switch
-                            checked={toggleInput?.shortToggle}
-                            name="shortToggle"
-                            onChange={handleChange}
-                            inputProps={{ 'aria-label': 'controlled' }}
-                        />} label="Short Stories" />
-                        <h4 style={{ display: 'inline', borderRadius: '50%', backgroundColor: '#18191B', padding: '5px 10px' }}>{shortStories.length}</h4>
-                    </div>
+                        <Button onClick={handleModalOpen} sx={{ color: '#bf483b' }} variant="inherit">Add New Blog</Button>
+
+                        <div style={{ display: 'inline-block', margin: '0 10px' }}>
+                            <FormControlLabel control={<Switch
+                                checked={toggleInput?.longToggle}
+                                name="longToggle"
+                                onChange={handleChange}
+                                inputProps={{ 'aria-label': 'controlled' }}
+                            />} label="Long Stories" />
+                            <h4 style={{ display: 'inline', borderRadius: '50%', backgroundColor: '#18191B', padding: '5px 10px' }}>{longStories.length}</h4>
+                        </div>
+                        <div style={{ display: 'inline-block', margin: '0 10px' }}>
+                            <FormControlLabel control={<Switch
+                                checked={toggleInput?.shortToggle}
+                                name="shortToggle"
+                                onChange={handleChange}
+                                inputProps={{ 'aria-label': 'controlled' }}
+                            />} label="Short Stories" />
+                            <h4 style={{ display: 'inline', borderRadius: '50%', backgroundColor: '#18191B', padding: '5px 10px' }}>{shortStories.length}</h4>
+                        </div>
+                    </Grid>
                 </Grid>
-
-            </Grid>
-            {/* <Box sx={{ width: '75%', mx: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center', my: 4, backgroundColor: '#242527', px: 5 }} >
-
-                <Button sx={{ mt: 0.5, fontWeight: 'bold' }} variant="inherit">
-                    <BookIcon sx={{ borderRadius: '50%', backgroundColor: '#3A3B3D', mr: 1, padding: '5px' }} />
-                    Blog</Button>
-
-                <Button onClick={handleDeleteAll} sx={{ mt: 0.5, mr: 1, color: '#bf483b' }} variant="inherit">Delete All</Button>
-                <FormControlLabel control={<Switch
-                    checked={toggleInput?.longToggle}
-                    name="longToggle"
-                    onChange={handleChange}
-                    inputProps={{ 'aria-label': 'controlled' }}
-                />} label="Long Stories" />
-                <h4 style={{ borderRadius: '50%', backgroundColor: '#18191B', padding: '5px 10px' }}>{longStories.length}</h4>
-                <FormControlLabel sx={{ ml: 4 }} control={<Switch
-                    checked={toggleInput?.shortToggle}
-                    name="shortToggle"
-                    onChange={handleChange}
-                    inputProps={{ 'aria-label': 'controlled' }}
-                />} label="Short Stories" />
-                <h4 style={{ borderRadius: '50%', backgroundColor: '#18191B', padding: '5px 10px' }}>{shortStories.length}</h4>
-            </Box> */}
-
-            {/* blogs grid  */}
-            <Grid sx={{ width: '80%', mx: 'auto', py: 4 }} container spacing={2}>
-                {
-                    (toggleInput.longToggle && toggleInput.shortToggle) && allStories.map(story => <Story
-                        key={story.id}
-                        story={story}>
-                    </Story>)
+                {success && <Alert severity="success">All blogs deleted successfully! Please Reload the page</Alert>
                 }
-                {
-                    (!toggleInput.longToggle && toggleInput.shortToggle) && shortStories.map(story => <Story
-                        key={story.id}
-                        story={story}>
-                    </Story>)
-                }
-                {
-                    (toggleInput.longToggle && !toggleInput.shortToggle) && longStories.map(story => <Story
-                        key={story.id}
-                        story={story}>
-                    </Story>)
-                }
-            </Grid>
-        </div >
+
+                {/* blogs grid  */}
+                <Grid sx={{ width: '80%', mx: 'auto', py: 4 }} container spacing={2}>
+                    {
+                        (toggleInput.longToggle && toggleInput.shortToggle) && allStories.map(story => <Story
+                            key={story.id}
+                            story={story}>
+                        </Story>)
+                    }
+                    {
+                        (!toggleInput.longToggle && toggleInput.shortToggle) && shortStories.map(story => <Story
+                            key={story.id}
+                            story={story}>
+                        </Story>)
+                    }
+                    {
+                        (toggleInput.longToggle && !toggleInput.shortToggle) && longStories.map(story => <Story
+                            key={story.id}
+                            story={story}>
+                        </Story>)
+                    }
+                </Grid>
+            </div >
+            <AddNewBlog
+                stories={allStories}
+                openModal={openModal}
+                handleModalClose={handleModalClose}
+            ></AddNewBlog>
+        </>
     );
 };
 
